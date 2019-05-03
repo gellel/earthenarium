3712a75d2b7854a298b020ad79f2a0740fd7bd3f
gellel <lindsay.gelle@gmail.com>
2019-05-04 00:34:26 +1000

    stash

--- main.py
+++ main.py
@@ -6,28 +6,93 @@ import collections
 import json
 # import os package
 import os
+# import regexp package
+import re
+# import system package
+import sys
 
-
-DEFAULT_ARGS = (
-    "config.json", os.path.dirname(os.path.abspath(__file__)))
+DIR = os.path.dirname(
+    os.path.abspath(__file__))
 
 DEFAULT_KEYS = (
-    "CONFIG","DIRECTORY",)
+    "DIRECTORY", "PROJECTION",)
+
+DEFAULT_PARAMETERS = (
+    os.path.join(DIR, "image"), "equirectangular")
 
 DEFAULT = collections.namedtuple(
-    *("DEFAULT", ' '.join(DEFAULT_KEYS)))(*DEFAULT_ARGS)
+    *("DEFAULT", ' '.join(DEFAULT_KEYS)))(*DEFAULT_PARAMETERS)
 
 
-class Cartography:
+class Cartography(object):
+    """Cartography holds projection information
+    read from a cartographic file"""
 
-    def __init__(self, config=None):            
-        with open(config) as f:
-            print(f)
+    @property
+    def DATUM(self):
+        return self.config.datum
+
+    @property
+    def DIMENSION(self):
+        return tuple(self.config.dimensions)
+
+    @property
+    def DPI(self):
+        return self.config.dpi
+
+    @property
+    def HEIGHT(self):
+        return self.config.height
+
+    @property
+    def KEYWORDS(self):
+        return self.config.keywords
+
+    @property
+    def KILOMETERS_PER_PIXEL(self):
+        return self.config.scale
+
+    @property
+    def MILES_PER_PIXEL(self):
+        return (self.config.scale / 1.609)
+    
+    @property
+    def MAX_EAST(self):
+        return self.config.east
+
+    @property
+    def MAX_NORTH(self):
+        return self.config.north
+
+    @property
+    def MAX_SOUTH(self):
+        return self.config.south
+
+    @property
+    def MAX_WEST(self):
+        return self.config.west
+
+    @property
+    def WIDTH(self):
+        return self.config.width
+
+    def __init__(self, config=None): 
+        try: 
+            with open(config) as f:
+                config = json.load(f)
+        except:
+            sys.exit(1)
+
+        self.config = collections.namedtuple(
+            *("config", ' '.join(config.keys())))(*config.values())
 
 
 if __name__ == "__main__":
 
-    configuration = os.path.join(DEFAULT.DIRECTORY, DEFAULT.CONFIG)
+    configuration = os.path.join(
+        DEFAULT.DIRECTORY, DEFAULT.PROJECTION, "config.json")
 
     cartography = Cartography(
             config=configuration)
+
+    print(cartography.MILES_PER_PIXEL)
