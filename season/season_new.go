e1e1dfe6dd33a9a53c909f2d6e14641dc29579f7
gellel <lindsay.gelle@gmail.com>
2019-05-14 14:29:14 +1000

    drying new method

--- season/season_new.go
+++ season/season_new.go
@@ -8,24 +8,16 @@ import (
 )
 
 func NewSeason(t *chronograph.Time, h *hemisphere.Hemisphere) *Season {
-
 	calendar := Hemisphere.Fetch(h)
 	term := calendar.Fetch(t.Month.Literal)
-
-	switch t.Month.Literal {
-	case time.December, time.January, time.February:
-		first := time.Date(t.Year.Number-1, term.Months[0], 1, 0, 0, 0, 0, t.Location)
-		begins := chronograph.NewTime(&first)
-		last := time.Date(begins.Year.Number+1, term.Months[2], 1, 0, 0, 0, 0, t.Location).AddDate(0, 1, 0).Add(time.Nanosecond * -1)
-		ends := chronograph.NewTime(&last)
-		return &Season{
-			Begins: begins,
-			Ends:   ends,
-			Term:   term}
+	year := t.Year.Number
+	month := t.Month.Literal
+	if month == time.December || month == time.January || month == time.February {
+		year = year - 1
 	}
-	first := time.Date(t.Year.Number, term.Months[0], 1, 0, 0, 0, 0, t.Location)
+	first := time.Date(year, term.Months[0], 1, 0, 0, 0, 0, t.Location)
 	begins := chronograph.NewTime(&first)
-	last := time.Date(begins.Year.Number, term.Months[2], 1, 0, 0, 0, 0, t.Location).AddDate(0, 1, 0).Add(time.Nanosecond * -1)
+	last := time.Date(begins.Year.Number, (term.Months[2] + 1), 1, 0, 0, 0, 0, t.Location).Add(time.Nanosecond * -1)
 	ends := chronograph.NewTime(&last)
 	return &Season{
 		Begins: begins,
